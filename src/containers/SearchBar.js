f509c21283f17617d6c218f7b50f99713f5247d9
Malgorzata <mkrzek@googlemail.com>
2018-02-12 18:48:30 +0000

    added corrected/added formReducer

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -1,51 +1,66 @@
-import React from 'react';
-import {Field, reduxForm} from 'redux-form';
-import {connect} from 'react-redux';
-import * as Actions from '../actions/index.js';
+import React from "react";
+import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import * as Actions from "../actions/index.js";
 
-class SearchBar extends React.Component{
-   
-    renderField=field=>{
-        const {input, label, type, meta:{error, touched}}= field;
-        return <fieldset className={`form-group ${touched && error ? "has-error" : ""}`}>
-            <div>
-              <input {...input} type={type} className="form-control" placeholder={label} />
-              <button type="submit" className="btn bmd-btn-icon">
-                <i className="material-icons">search</i>
-              </button>
-              {touched && error && <div className="alert alert-danger">
-                    {error}{" "}
-                  </div>}
-            </div>
-          </fieldset>;
-    }
+class SearchBar extends React.Component {
+  renderField = field => {
+    const { input, type, label, meta: { touched, error } } = field;
+    return (
+      <fieldset className={`form-group${touched && error ? "has-error" : ""}`}>
+        <div>
+          <div className="row justify-content-center">
+            <input
+              {...input}
+              type={type}
+              placeholder={label}
+              className="col-7 form-control text-lowercase searchBar"
+            />
+            <button type="submit" className="btn bmd-btn-icon">
+              <i className="material-icons">search</i>
+            </button>
+          </div>
 
-    submitQuery=values=>{
-        this.props.submitQuery(values)
-    }
+          {touched &&
+            error && <div className="alert alert-danger">{error} </div>}
+        </div>
+      </fieldset>
+    );
+  };
 
-    render(){
-        const {handleSubmit}=this.props;
-        return <form onSubmit={handleSubmit(this.submitQuery)}>
-                <Field 
-                 name='searchBar'
-                 label='Enter a location'
-                 type='text'
-                 component={this.renderField}/>
-                
-            </form>
-        
-    }
+  submitForm = values => {
+    this.props.fetchDfTags(values);
+    values.searchBar = "";
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.props.handleSubmit(this.submitForm)}>
+          <Field
+            name="searchBar"
+            type="text"
+            label="Enter a query"
+            component={this.renderField}
+          />
+        </form>
+      </div>
+    );
+  }
 }
-function validate(values){
-    const errors={}
-    if (!values.name){
-        errors.name='Plase enter a valid location'
-    }
-    return errors;
+
+function validate(values) {
+  const errors = {};
+  if (!values.searchBar) {
+    errors.searchBar = "Enter your query";
+  }
+
+  return errors;
 }
 
-export default connect(null, Actions)(reduxForm({
-    form: 'SearchBar',
-    validate
-})(SearchBar));
+export default connect(null, Actions)(
+  reduxForm({
+    validate,
+    form: "searchBar"
+  })(SearchBar)
+);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,12 @@
 import { combineReducers } from "redux";
 import { routerReducer } from "react-router-redux";
-
+import { reducer as FormReducer } from "redux-form";
 
 import Weather from './reducer_weather.js';
 import Quote from './reducer_quote.js';
 
 const rootReducer = combineReducers({
-
+form: FormReducer,
 weather: Weather,
 quote: Quote,
 
